870fb68e24cdf64f09a36f1c327644f3cf167513
Igor Mouawad <igor@mouawad.com.br>
2023-09-21 16:20:51 -0300

    Estado e Cidade

---
+++ src/main/java/com/mouawad/cursomc/domain/Cidade.java
@@ -0,0 +1,51 @@
+package com.mouawad.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Cidade implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+
+	@ManyToOne
+	@JoinColumn(name="estado_id")
+	private Estado estado;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cidade other = (Cidade) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
--- src/main/java/com/mouawad/cursomc/resources/excepetion/StandardError.java
+++ src/main/java/com/mouawad/cursomc/resources/excepetion/StandardError.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @Setter
 public class StandardError implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer status;
 	private String msg;
 	private Long timeStamp;
